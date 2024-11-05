bbfd8ab46f0765579aff26e5609a9c83fd4494a2
Heza GÖLCÜK <h.golcuk91@outlook.com>
2024-11-06 01:04:38 +0300

    react homework-02 started

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+import Feedback from '../FeedbackFile/Feedback.jsx';
+
+function App() {
+  
+
+  return (
+    <>
+      <Feedback />
+    </>
+  );
+}
+
+export default App;
